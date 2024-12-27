d87ad57e24fbe3aea244329394e73ef2eac2f047
Harshabellala26 <harshabellala23@gmail.com>
2024-12-27 16:05:29 +0530

    Add files via upload

---
+++ backend/config/connection.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const JWT_SECRET = "secret";
+
+// MongoDB connection URL
+const mongoURI = "mongodb://127.0.0.1:27017/paytm";
+
+mongoose.set("strictQuery", false); 
+
+const dbconnect = mongoose
+  .connect(mongoURI)
+  .then(() => {
+    console.log("Database connected successfully");
+  })
+  .catch((err) => {
+    console.error("Database connection error:", err);
+  });
+
+module.exports = { dbconnect, JWT_SECRET };
